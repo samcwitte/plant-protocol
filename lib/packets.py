4a9071174fd86084ccde0f8192940925c98ec47a
SnFoil <samwitte@me.com>
2023-10-26 11:57:35 -0500

    Update packets.py

--- lib/packets.py
+++ lib/packets.py
@@ -1,17 +1,18 @@
 class IP_Addr:
     def __init__(self, ip_address):
         self.ip = ip_address.split(".") # i.e. ['127', '0', '0', '12']
-        self.ip[0] = int(self.ip[0])
-        self.ip[1] = int(self.ip[1])
-        self.ip[2] = int(self.ip[2])
-        self.ip[3] = int(self.ip[3])
+        for i in range(4):
+            self.ip[i] = int(self.ip[i])
         
     def toHex(self):
         hex = '{:02x}'.format(self.ip[0]) + '{:02x}'.format(self.ip[1]) + '{:02x}'.format(self.ip[2]) + '{:02x}'.format(self.ip[3])
         return hex
     
-    def stringify(ip_to_convert):
-        ip_string = str(int(ip_to_convert[0:2], 16)) + "." + str(int(ip_to_convert[2:4], 16)) + "." + str(int(ip_to_convert[4:6], 16)) + "." + str(int(ip_to_convert[6:8], 16))
+    def stringify(ip):
+        ip_string = str(int(ip[0:2], 16)) + "." + \
+                    str(int(ip[2:4], 16)) + "." + \
+                    str(int(ip[4:6], 16)) + "." + \
+                    str(int(ip[6:8], 16))
         return ip_string
 
 class Packet:
@@ -23,10 +24,16 @@ class Packet:
         self.data = data
             
     def toBytes(self):
-        byteString = '{:08x}'.format(self.client_id) + self.dest_ip.toHex() + '{:08x}'.format(self.packet_id) + '{:08x}'.format(self.data_length) + str(self.data) + "\n"
+        # Converts data into a bytestring format for transmission between client and server
+        byteString = '{:08x}'.format(self.client_id) + \
+                     self.dest_ip.toHex() + \
+                     '{:08x}'.format(self.packet_id) + \
+                     '{:08x}'.format(self.data_length) + \
+                     str(self.data) + "\n"
         return bytes(byteString, 'utf-8')
 
     def summary(packet):
+        # Prints the packet (packet needs to be in bytestring form)
         client_id = packet[0:8]
         ip = packet[8:16] # IP Address is 4 bytes, which is 8 hex digits
         packet_id = packet[16:24]
